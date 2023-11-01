2c159a00ae17040f1739f3b8f319e50d6aeec054
Rafael Vilches <rvilches38@yahoo.com.mx>
2023-11-01 18:47:51 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const commentsCtrl = require('../controllers/comments');
+
+router.get('/', commentsCtrl.index);
+router.post('/', commentsCtrl.create);
+router.get('/new', commentsCtrl.new);
+router.get('/:id', commentsCtrl.show);
+router.delete('/:id', commentsCtrl.delete);
+router.get('/:id/edit', commentsCtrl.edit);
+router.put('/:id', commentsCtrl.update);
+
+module.exports = router;
